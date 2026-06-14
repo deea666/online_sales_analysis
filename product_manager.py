a970106245b18542250f4f92aac06e043a8fdc37
deea666 <voicu.andreea0508@gmail.com>
2026-06-14 17:43:29 +0300

    product_manager.py

--- product_manager.py
+++ product_manager.py
@@ -23,3 +23,9 @@ class ProductManager:
             total += product.price * product.quantity
 
         return total
+        def remove_product(self, name):
+    self.products = [
+        product
+        for product in self.products
+        if product.name != name
+    ]
